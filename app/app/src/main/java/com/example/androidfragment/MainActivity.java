5435e48491b69988c9c8873b8346944664f4a4c2
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-04 14:49:39 +0530

    Refector Layout For Manual Fragment Insertion

--- app/app/src/main/java/com/example/androidfragment/MainActivity.java
+++ app/app/src/main/java/com/example/androidfragment/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.androidfragment;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,17 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // programmatically creating fragment and inject to main_activity XML layout
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        // Getting The Fragment Where we wanna inject our fragment.
-        Fragment fragment = fragmentManager.findFragmentById(R.id.my_container);
-
-        if(fragment == null) {
-            // this new CourseListFragment() is our created Class. see class for more detail. it has the override method
-            fragment = new CourseListFragment();
-            fragmentManager.beginTransaction().add(R.id.my_container, fragment)
-            .commit();
-        }
     }
 }
