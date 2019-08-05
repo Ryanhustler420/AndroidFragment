25280168eb622236e9205d1426789652e2902ac6
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-05 12:35:06 +0530

    Setup OnItemClick Listener - Take to Details Activity

--- app/app/src/main/java/com/example/androidfragment/CourseListFragment.java
+++ app/app/src/main/java/com/example/androidfragment/CourseListFragment.java
@@ -1,5 +1,7 @@
 package com.example.androidfragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.androidfragment.data.Course;
@@ -18,6 +21,7 @@ import java.util.List;
 
 public class CourseListFragment extends ListFragment{
     List<Course> courses = new CourseData().courseList();
+    private Callbacks activity;
     // we need constructor because when user change orientation of device
     // where this helps else application could crash.
     public CourseListFragment() { }
@@ -46,5 +50,35 @@ public class CourseListFragment extends ListFragment{
         return view;
 
         // return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    // we created this
+    public interface  Callbacks {
+        void onItemSelected(Course course);
+    }
+
+    // we have to override this method
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        //  super.onListItemClick(l, v, position, id);
+        //  Toast.makeText(getActivity(), "position " + position, Toast.LENGTH_LONG).show();
+
+        Course course = courses.get(position);
+
+        // we can not start an activity since we are inside an fragment.
+        // startActivity(new Intent(this, CourseDetailActivity.class));
+
+        // we are passing to this interface function.
+        this.activity.onItemSelected(course);
+        // we have to implement fragment life cycle hooks
+
+        // Toast.makeText(getActivity(), "Name: " + course.getCourseName(), Toast.LENGTH_LONG).show();
+    }
+
+    // life cycle hook
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.activity = (Callbacks) context;
     }
 }
--- app/app/src/main/java/com/example/androidfragment/MainActivity.java
+++ app/app/src/main/java/com/example/androidfragment/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.androidfragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.androidfragment.data.Course;
+
+public class MainActivity extends AppCompatActivity implements CourseListFragment.Callbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemSelected(Course course) {
+        Toast.makeText(getApplicationContext(), course.getCourseName(), Toast.LENGTH_LONG).show();
     }
 }
