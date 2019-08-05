f736c4eed4794991469a44c4c417ac1c7e0d1eb6
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-05 11:49:07 +0530

    Setup Details Activity And Design Layout

--- app/app/src/main/java/com/example/androidfragment/CourseDetailActivity.java
+++ app/app/src/main/java/com/example/androidfragment/CourseDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidfragment;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class CourseDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
+
+        if (savedInstanceState == null) {
+            CourseDetailFragment fragment = new CourseDetailFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.detailContainer, fragment)
+                    .commit();
+        }
     }
 }
--- app/app/src/main/java/com/example/androidfragment/CourseDetailFragment.java
+++ app/app/src/main/java/com/example/androidfragment/CourseDetailFragment.java
@@ -1,7 +1,40 @@
 package com.example.androidfragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.androidfragment.data.Course;
 
 public class CourseDetailFragment extends Fragment {
+    Course course;
 
+    // Require no-args constructor
+    public CourseDetailFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.course_detail_fragment, container, false);
+
+        if(course != null) {
+            TextView courseName = view.findViewById(R.id.detailCourseName);
+            courseName.setText(course.getCourseName());
+
+            ImageView courseImage = view.findViewById(R.id.detailCourseImage);
+            courseImage.setImageResource(course.getImageResourceId(getActivity()));
+
+            TextView courseDescription = view.findViewById(R.id.detailCourseDescription);
+            courseDescription.setText(course.getCourseName());
+        }
+
+        return view;
+    }
 }
