eda49b1ff51c74f04feb22e6d7392c4c8df4751c
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-05 14:58:08 +0530

    Conditionally Check And Using Fragment
    Checking if sw600 layout rander than we'll use fragment else simple intent

--- app/app/src/main/java/com/example/androidfragment/MainActivity.java
+++ app/app/src/main/java/com/example/androidfragment/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.androidfragment;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.androidfragment.data.Course;
 
 public class MainActivity extends AppCompatActivity implements CourseListFragment.Callbacks {
 
+    private boolean isTwoPane = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.detailContainer) != null){
+            isTwoPane = true;
+        }
     }
 
     @Override
     public void onItemSelected(Course course, int position) {
-        Toast.makeText(getApplicationContext(), course.getCourseName(), Toast.LENGTH_LONG).show();
+        if(isTwoPane) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("course_id", position);
 
-        Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
-        intent.putExtra("course_id", position);
-        startActivity(intent);
+            FragmentManager fm = getSupportFragmentManager();
+            CourseDetailFragment courseDetailFragment = new CourseDetailFragment();
+
+            courseDetailFragment.setArguments(bundle);
+
+            fm.beginTransaction()
+                    .replace(R.id.detailContainer, courseDetailFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
+            intent.putExtra("course_id", position);
+            startActivity(intent);
+        }
     }
 }
